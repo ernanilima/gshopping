67c97fb90c8617643e69f911c1fd5694a965bc1a
ernanilima <ernani.tecc@gmail.com>
2023-02-26 20:15:09 -0300

    Teste para banner

--- app/config/banner_test.go
+++ app/config/banner_test.go
@@ -1,72 +1,72 @@
 package config_test
 
-// import (
-// 	"fmt"
-// 	"os"
-// 	"runtime"
-// 	"testing"
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"testing"
 
-// 	"github.com/ernanilima/gshopping/app/config"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/ernanilima/gshopping/app/config"
+	"github.com/stretchr/testify/assert"
+)
 
-// var configs = config.Config{
-// 	Server: struct {
-// 		Port    int    `mapstructure:"port"`
-// 		Version string `mapstructure:"version"`
-// 	}{
-// 		Port:    8989,
-// 		Version: "1.0",
-// 	},
-// }
+var configs = config.Config{
+	Server: struct {
+		Port    int    `mapstructure:"port"`
+		Version string `mapstructure:"version"`
+	}{
+		Port:    8989,
+		Version: "1.0",
+	},
+}
 
-// // Deve imprimir um erro por nao encontrar o banner
-// func TestStartBanner_Should_Print_A_Error_For_Not_Finding_The_Banner(t *testing.T) {
-// 	// capturar a saida do metodo
-// 	output := CaptureOutput(func() {
-// 		config.StartBanner(configs)
-// 	})
+// Deve imprimir um erro por nao encontrar o banner
+func TestStartBanner_Should_Print_A_Error_For_Not_Finding_The_Banner(t *testing.T) {
+	// capturar a saida do metodo
+	output := CaptureOutput(func() {
+		configs.StartBanner()
+	})
 
-// 	assert.Contains(t, output, "Erro ao ler arquivo banner.txt")
-// }
+	assert.Contains(t, output, "Erro ao ler arquivo banner.txt")
+}
 
-// // Deve imprimir o banner
-// func TestStartBanner_Should_Print_The_Banner(t *testing.T) {
-// 	bannerForTest("CREATE")
+// Deve imprimir o banner
+func TestStartBanner_Should_Print_The_Banner(t *testing.T) {
+	bannerForTest("CREATE")
 
-// 	// capturar a saida do metodo
-// 	output := CaptureOutput(func() {
-// 		config.StartBanner(configs)
-// 	})
+	// capturar a saida do metodo
+	output := CaptureOutput(func() {
+		configs.StartBanner()
+	})
 
-// 	bannerForTest("DELETE")
+	bannerForTest("DELETE")
 
-// 	expected := fmt.Sprintf("Banner - :: Go: %s :: Porta: %d (v%s)\n",
-// 		runtime.Version(),
-// 		configs.Server.Port,
-// 		configs.Server.Version)
+	expected := fmt.Sprintf("Banner - :: Go: %s :: Porta: %d (v%s)\n",
+		runtime.Version(),
+		configs.Server.Port,
+		configs.Server.Version)
 
-// 	assert.Equal(t, expected, output)
-// }
+	assert.Equal(t, expected, output)
+}
 
-// func bannerForTest(action string) {
-// 	filename := "banner.txt"
-// 	if action == "CREATE" {
-// 		content := []byte("Banner - :: Go: %s :: Porta: %d (v%s)")
-// 		file, err := os.Create(filename)
-// 		if err != nil {
-// 			fmt.Println("Erro ao criar o arquivo:", err)
-// 			return
-// 		}
-// 		defer file.Close()
-// 		if _, err = file.Write(content); err != nil {
-// 			fmt.Println("Erro ao escrever no arquivo:", err)
-// 			return
-// 		}
-// 	} else if action == "DELETE" {
-// 		if os.Remove(filename) != nil {
-// 			fmt.Println("Erro ao apagar o arquivo:")
-// 			return
-// 		}
-// 	}
-// }
+func bannerForTest(action string) {
+	filename := "banner.txt"
+	if action == "CREATE" {
+		content := []byte("Banner - :: Go: %s :: Porta: %d (v%s)")
+		file, err := os.Create(filename)
+		if err != nil {
+			fmt.Println("Erro ao criar o arquivo:", err)
+			return
+		}
+		defer file.Close()
+		if _, err = file.Write(content); err != nil {
+			fmt.Println("Erro ao escrever no arquivo:", err)
+			return
+		}
+	} else if action == "DELETE" {
+		if os.Remove(filename) != nil {
+			fmt.Println("Erro ao apagar o arquivo:")
+			return
+		}
+	}
+}
