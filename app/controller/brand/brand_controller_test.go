42411477ee9a83de5a97ba74e5031e5d0daf0359
ernanilima <ernani.tecc@gmail.com>
2023-03-04 20:29:16 -0300

    Teste para brand controller buscar por descricao

--- app/controller/brand/brand_controller_test.go
+++ app/controller/brand/brand_controller_test.go
@@ -65,3 +65,29 @@ func TestFindBrandById_Should_Return_Status_200_To_Fetch_A_Brand_By_ID(t *testin
 	// verifica se a resposta HTTP eh 200
 	assert.Equal(t, http.StatusOK, res.Code)
 }
+
+// Deve retornar o status 200 para buscar todas as Marcas por descricao
+func TestFindAllBrandsByDescription_Should_Return_Status_200_To_Fetch_All_Brands_By_Description(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mocks.NewMockRepository(ctrl)
+	controller := controller.NewController(repository)
+
+	repository.EXPECT().FindByDescription(gomock.Any(), gomock.Any()).Return(utils.Pageable{TotalElements: 10}, nil)
+
+	r := router.StartRoutes(controller)
+
+	// cria uma requisicao HTTP GET para "/v1/produto"
+	req, err := http.NewRequest("GET", "/v1/marca/descricao/descri", nil)
+	assert.NoError(t, err)
+
+	// cria um HTTP recorder para receber a resposta
+	res := httptest.NewRecorder()
+
+	// executa a requisicao no router
+	r.ServeHTTP(res, req)
+
+	// verifica se a resposta HTTP eh 200
+	assert.Equal(t, http.StatusOK, res.Code)
+}
