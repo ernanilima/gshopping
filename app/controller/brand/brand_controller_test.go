d4817540cb6fa6fc4bf0f147dccf5c10da38b92e
ernanilima <ernani.tecc@gmail.com>
2023-03-04 20:28:45 -0300

    Teste para brand controller buscar por id

--- app/controller/brand/brand_controller_test.go
+++ app/controller/brand/brand_controller_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/ernanilima/gshopping/app/controller"
+	"github.com/ernanilima/gshopping/app/model"
 	"github.com/ernanilima/gshopping/app/router"
 	"github.com/ernanilima/gshopping/app/test/mocks"
 	"github.com/ernanilima/gshopping/app/utils"
@@ -27,6 +28,32 @@ func TestFindAllBrands_Should_Return_Status_200_To_Fetch_All_Brands(t *testing.T
 
 	// cria uma requisicao HTTP GET para "/v1/produto"
 	req, err := http.NewRequest("GET", "/v1/marca", nil)
+	assert.NoError(t, err)
+
+	// cria um HTTP recorder para receber a resposta
+	res := httptest.NewRecorder()
+
+	// executa a requisicao no router
+	r.ServeHTTP(res, req)
+
+	// verifica se a resposta HTTP eh 200
+	assert.Equal(t, http.StatusOK, res.Code)
+}
+
+// Deve retornar o status 200 para buscar uma marca por id
+func TestFindBrandById_Should_Return_Status_200_To_Fetch_A_Brand_By_ID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mocks.NewMockRepository(ctrl)
+	controller := controller.NewController(repository)
+
+	repository.EXPECT().FindById(gomock.Any()).Return(model.Brand{}, nil)
+
+	r := router.StartRoutes(controller)
+
+	// cria uma requisicao HTTP GET para "/v1/produto"
+	req, err := http.NewRequest("GET", "/v1/marca/6f75b5bc-e561-4bc7-a28d-e74bc706a4e9", nil)
 	assert.NoError(t, err)
 
 	// cria um HTTP recorder para receber a resposta
