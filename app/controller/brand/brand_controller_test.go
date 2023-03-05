a6894f1dda18a666a9e17535dbac258664267d6e
ernanilima <ernani.tecc@gmail.com>
2023-03-05 20:28:31 -0300

    Atualizado teste para buscar todas as marcas

--- app/controller/brand/brand_controller_test.go
+++ app/controller/brand/brand_controller_test.go
@@ -1,33 +1,54 @@
 package brand_controller_test
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/ernanilima/gshopping/app/controller"
 	"github.com/ernanilima/gshopping/app/model"
 	"github.com/ernanilima/gshopping/app/router"
 	"github.com/ernanilima/gshopping/app/test/mocks"
 	"github.com/ernanilima/gshopping/app/utils"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
+
+var brands = []model.Brand{
+	{
+		ID:          uuid.New(),
+		Description: "Marda para teste 1",
+		CreatedAt:   time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC),
+	},
+	{
+		ID:          uuid.New(),
+		Description: "Marda para teste 2",
+		CreatedAt:   time.Date(2022, time.February, 2, 22, 32, 42, 0, time.UTC),
+	},
+}
 
 // Deve retornar o status 200 para buscar todas as marcas
 func TestFindAllBrands_Should_Return_Status_200_To_Fetch_All_Brands(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-
 	repository := mocks.NewMockRepository(ctrl)
 	controller := controller.NewController(repository)
-
-	repository.EXPECT().FindAll(gomock.Any()).Return(utils.Pageable{})
+	repository.EXPECT().FindAll(gomock.Any()).Return(utils.Pageable{
+		Content:          brands,
+		TotalPages:       0,           // total de paginas
+		TotalElements:    len(brands), // total de entidades localizadas
+		Size:             10,          // total de entidades por pagina
+		Page:             0,           // pagina atual
+		NumberOfElements: len(brands), // total de entidades por pagina
+	})
 
 	r := router.StartRoutes(controller)
 
-	// cria uma requisicao HTTP GET para "/v1/produto"
-	req, err := http.NewRequest("GET", "/v1/marca", nil)
+	// cria uma requisicao HTTP GET para "/v1/marca"
+	req, err := http.NewRequest("GET", "/v1/marca?size=12&page=0&sort=id,asc", nil)
 	assert.NoError(t, err)
 
 	// cria um HTTP recorder para receber a resposta
@@ -36,8 +57,18 @@ func TestFindAllBrands_Should_Return_Status_200_To_Fetch_All_Brands(t *testing.T
 	// executa a requisicao no router
 	r.ServeHTTP(res, req)
 
-	// verifica se a resposta HTTP eh 200
+	var result utils.Pageable
+	err = json.Unmarshal(res.Body.Bytes(), &result)
+	assert.NoError(t, err)
+
+	// verifica os resultados
 	assert.Equal(t, http.StatusOK, res.Code)
+	assert.NotNil(t, result.Content)
+	assert.Equal(t, 0, result.TotalPages)
+	assert.Equal(t, 2, result.TotalElements)
+	assert.Equal(t, 10, result.Size)
+	assert.Equal(t, 0, result.Page)
+	assert.Equal(t, 2, result.NumberOfElements)
 }
 
 // Deve retornar o status 200 para buscar uma marca por id
