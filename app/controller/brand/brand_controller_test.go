4b01aa8933ba9757b29c81db72a620885c7be79a
ernanilima <ernani.tecc@gmail.com>
2023-03-05 20:32:38 -0300

    Atualizado teste para buscar marcas por descricao

--- app/controller/brand/brand_controller_test.go
+++ app/controller/brand/brand_controller_test.go
@@ -102,20 +102,25 @@ func TestFindBrandById_Should_Return_Status_200_To_Fetch_A_Brand_By_ID(t *testin
 	assert.Equal(t, time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC), result.CreatedAt)
 }
 
-// Deve retornar o status 200 para buscar todas as Marcas por descricao
-func TestFindAllBrandsByDescription_Should_Return_Status_200_To_Fetch_All_Brands_By_Description(t *testing.T) {
+// Deve retornar o status 200 para buscar marcas por descricao
+func TestFindAllBrandsByDescription_Should_Return_Status_200_To_Fetch_Brands_By_Description(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-
 	repository := mocks.NewMockRepository(ctrl)
 	controller := controller.NewController(repository)
-
-	repository.EXPECT().FindByDescription(gomock.Any(), gomock.Any()).Return(utils.Pageable{TotalElements: 10}, nil)
+	repository.EXPECT().FindByDescription(gomock.Any(), gomock.Any()).Return(utils.Pageable{
+		Content:          brands,
+		TotalPages:       0,           // total de paginas
+		TotalElements:    len(brands), // total de entidades localizadas
+		Size:             10,          // total de entidades por pagina
+		Page:             0,           // pagina atual
+		NumberOfElements: len(brands), // total de entidades por pagina
+	}, nil)
 
 	r := router.StartRoutes(controller)
 
-	// cria uma requisicao HTTP GET para "/v1/produto"
-	req, err := http.NewRequest("GET", "/v1/marca/descricao/descri", nil)
+	// cria uma requisicao HTTP GET para "/v1/marca/descricao/{description}"
+	req, err := http.NewRequest("GET", "/v1/marca/descricao/Marca", nil)
 	assert.NoError(t, err)
 
 	// cria um HTTP recorder para receber a resposta
@@ -124,6 +129,16 @@ func TestFindAllBrandsByDescription_Should_Return_Status_200_To_Fetch_All_Brands
 	// executa a requisicao no router
 	r.ServeHTTP(res, req)
 
-	// verifica se a resposta HTTP eh 200
+	var result utils.Pageable
+	err = json.Unmarshal(res.Body.Bytes(), &result)
+	assert.NoError(t, err)
+
+	// verifica os resultados
 	assert.Equal(t, http.StatusOK, res.Code)
+	assert.NotNil(t, result.Content)
+	assert.Equal(t, 0, result.TotalPages)
+	assert.Equal(t, 2, result.TotalElements)
+	assert.Equal(t, 10, result.Size)
+	assert.Equal(t, 0, result.Page)
+	assert.Equal(t, 2, result.NumberOfElements)
 }
