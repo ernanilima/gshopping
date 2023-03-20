e07e683d92f11f9a3873a2fa5fa62c588469dd20
ernanilima <ernani.tecc@gmail.com>
2023-03-19 21:52:11 -0300

    Teste para request json com sucesso

---
+++ app/utils/response/response_test.go
@@ -0,0 +1,50 @@
+package response_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/ernanilima/gshopping/app/model"
+	"github.com/ernanilima/gshopping/app/utils/response"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+var brands = []model.Brand{
+	{
+		ID:          uuid.New(),
+		Description: "Marda para teste 1",
+		CreatedAt:   time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC),
+	},
+	{
+		ID:          uuid.New(),
+		Description: "Marda para teste 2",
+		CreatedAt:   time.Date(2022, time.February, 2, 22, 32, 42, 0, time.UTC),
+	},
+}
+
+// Deve retornar um request usado para sucesso
+func TestJSON_Should_Return_A_Request_For_Success(t *testing.T) {
+	res := httptest.NewRecorder()
+
+	response.JSON(res, http.StatusOK, brands)
+
+	var result []model.Brand
+	err := json.Unmarshal(res.Body.Bytes(), &result)
+	assert.NoError(t, err)
+
+	// verifica os resultados
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Equal(t, 1, len(res.Header()))
+	assert.Equal(t, "application/json", res.Header().Get("Content-Type"))
+	assert.Equal(t, 2, len(result))
+	assert.NotNil(t, result[0].ID)
+	assert.Equal(t, "Marda para teste 1", result[0].Description)
+	assert.Equal(t, time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC), result[0].CreatedAt)
+	assert.NotNil(t, result[1].ID)
+	assert.Equal(t, "Marda para teste 2", result[1].Description)
+	assert.Equal(t, time.Date(2022, time.February, 2, 22, 32, 42, 0, time.UTC), result[1].CreatedAt)
+}
