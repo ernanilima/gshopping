11b4de28a8091f99bfd21756b58f6f887c48c3c0
ernanilima <ernani.tecc@gmail.com>
2023-03-28 20:19:36 -0300

    Funcao sql para buscar id da marca por descricao

---
+++ db/postgres/202303261949_get_id_by_brand_description.sql
@@ -0,0 +1,5 @@
+-- +goose Up
+CREATE FUNCTION get_brand_id_by_description(v VARCHAR) RETURNS UUID AS $$ BEGIN RETURN id FROM brand WHERE UPPER(unaccent(description)) = UPPER(unaccent(v)); END; $$ LANGUAGE plpgsql;
+
+-- +goose Down
+DROP FUNCTION IF EXISTS get_brand_id_by_description;
