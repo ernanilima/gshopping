8b37794727946454b27b83e82b972edd73af13d7
ernanilima <ernani.tecc@gmail.com>
2023-04-01 19:38:58 -0300

    Teste para product no controller generico

--- app/controller/controller_test.go
+++ app/controller/controller_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ernanilima/gshopping/app/config"
 	"github.com/ernanilima/gshopping/app/controller"
 	brand_controller "github.com/ernanilima/gshopping/app/controller/brand"
+	product_controller "github.com/ernanilima/gshopping/app/controller/product"
 	"github.com/ernanilima/gshopping/app/repository"
 	"github.com/ernanilima/gshopping/app/repository/database"
 	"github.com/stretchr/testify/assert"
@@ -16,6 +17,35 @@ import (
 func TestController_Should_Return_The_Total_Methods(t *testing.T) {
 	totalMethods := reflect.TypeOf((*controller.Controller)(nil)).Elem().NumMethod()
 	assert.Equal(t, 4, totalMethods)
+}
+
+// Deve retornar os metodos para a interface product controller
+func TestNewController_Should_Return_Methods_For_ProductController(t *testing.T) {
+	databaseConfig := &database.DatabaseConfig{Config: &config.Config{}}
+	controller := controller.NewController(repository.NewRepository(databaseConfig))
+
+	result, exist := controller.(product_controller.ProductController)
+	assert.True(t, exist)
+	productControllerTypeOf := reflect.TypeOf((*product_controller.ProductController)(nil)).Elem()
+	controllerTypeOf := reflect.TypeOf(result)
+	assert.NotNil(t, controllerTypeOf)
+
+	nameOfMethods := []string{
+		"FindProductByBarcode",
+	}
+
+	assert.Equal(t, len(nameOfMethods), productControllerTypeOf.NumMethod())
+	for _, name := range nameOfMethods {
+		// metodos existentes no controlador generico
+		method, exist := controllerTypeOf.MethodByName(name)
+		assert.True(t, exist)
+		assert.Equal(t, name, method.Name)
+
+		// metodos existentes no controlador especifico
+		method, exist = productControllerTypeOf.MethodByName(name)
+		assert.True(t, exist)
+		assert.Equal(t, name, method.Name)
+	}
 }
 
 // Deve retornar os metodos para a interface brand controller
