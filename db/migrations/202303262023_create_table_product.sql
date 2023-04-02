434e3c8d169ed06505f46e8db22f19762dbf9506
ernanilima <ernani.tecc@gmail.com>
2023-04-02 18:11:02 -0300

    Atualizado migracao para inserir mais produtos

--- db/migrations/202303262023_create_table_product.sql
+++ db/migrations/202303262023_create_table_product.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS product (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     barcode VARCHAR(14) NOT NULL UNIQUE,
-    description VARCHAR(50) NOT NULL UNIQUE,
+    description VARCHAR(50) NOT NULL,
     brand_id UUID NOT NULL,
     created_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_product_brand FOREIGN KEY (brand_id) REFERENCES public.brand(id)
--- db/migrations/202303262041_insert_table_product.sql
+++ db/migrations/202303262041_insert_table_product.sql
@@ -1,2 +1,12 @@
 -- +goose Up
 INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('789102030', 'Produto De Teste', get_brand_id_by_description('1906'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('8412598001079', 'Cerveja Vienna Lager Red Vintage 1906 Ga', get_brand_id_by_description('1906'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005802892', 'Cappuccino Solúvel Chocolate 3 Corações ', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005800676', 'Café Solúvel Granulado Tradicional 3 Cor', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005800690', 'Café Solúvel Granulado Descafeinado 3 Co', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005800683', 'Café Solúvel Granulado Tradicional 3 Cor', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005800706', 'Café Solúvel Granulado Descafeinado 3 Co', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005802403', 'Cappuccino Solúvel Baunilha 3 Corações S', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005802427', 'Cappuccino Solúvel Canela 3 Corações Sac', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005802397', 'Cappuccino Solúvel Chocolate 3 Corações ', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
+INSERT INTO product (barcode, description, brand_id, created_at) VALUES ('7896005800157', 'Cappuccino Solúvel Classic 3 Corações Po', get_brand_id_by_description('3 Corações'), LOCALTIMESTAMP);
