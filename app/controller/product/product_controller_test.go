482e2651978649c5677e2c7898f0b32fd67c292e
ernanilima <ernani.tecc@gmail.com>
2023-04-07 18:55:04 -0300

    Teste para buscar produto por codigo de barras com erro no controller

--- app/controller/product/product_controller_test.go
+++ app/controller/product/product_controller_test.go
@@ -2,6 +2,7 @@ package product_controller_test
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,6 +12,7 @@ import (
 	"github.com/ernanilima/gshopping/app/model"
 	"github.com/ernanilima/gshopping/app/router"
 	"github.com/ernanilima/gshopping/app/test/mocks"
+	"github.com/ernanilima/gshopping/app/utils/response"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
@@ -64,4 +66,37 @@ func TestFindProductByBarcode_Should_Return_Status_200_To_Fetch_A_Product_By_Bar
 	assert.Equal(t, "Produto para teste 1", result.Description)
 	assert.Equal(t, "Marda para teste 1", result.Brand)
 	assert.Equal(t, time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC), result.CreatedAt)
+}
+
+// Deve retornar o status 404 para buscar um produto por codigo de barras quando nao localizar nenhum produto
+func TestFindProductByBarcode_Should_Return_Status_404_To_Fetch_A_Product_By_Barcode_When_No_Product_Is_Found(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	repository := mocks.NewMockRepository(ctrl)
+	controller := controller.NewController(repository)
+	repository.EXPECT().FindByBarcode(gomock.Any()).Return(model.Product{}, errors.New("Error"))
+
+	r := router.StartRoutes(controller)
+
+	// cria uma requisicao HTTP GET para "/v1/produto/{barcode}"
+	req, err := http.NewRequest("GET", "/v1/produto/7891020301", nil)
+	assert.NoError(t, err)
+
+	// cria um HTTP recorder para receber a resposta
+	res := httptest.NewRecorder()
+
+	// executa a requisicao no router
+	r.ServeHTTP(res, req)
+
+	var result response.StandardError
+	err = json.Unmarshal(res.Body.Bytes(), &result)
+	assert.NoError(t, err)
+
+	// verifica os resultados
+	assert.Equal(t, http.StatusNotFound, res.Code)
+	assert.NotNil(t, result.Timestamp)
+	assert.Equal(t, res.Code, result.Status)
+	assert.Equal(t, http.StatusText(res.Code), result.Error)
+	assert.Equal(t, "Produto não encontrado", result.Message)
+	assert.Equal(t, "/v1/produto/7891020301", result.Path)
 }
