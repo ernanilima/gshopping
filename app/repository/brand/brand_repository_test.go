27501e949b22b130e4e32a7d5cb66faff6e354ed
ernanilima <ernani.tecc@gmail.com>
2023-04-09 18:58:44 -0300

    Teste para buscar marca por id no depository

--- app/repository/brand/brand_repository_test.go
+++ app/repository/brand/brand_repository_test.go
@@ -30,8 +30,8 @@ func TestFindAll_Should_Return_All_Brands(t *testing.T) {
 
 	// cria um mock dos dados que deveram ser retornados
 	rows := sqlmock.NewRows([]string{"count", "id", "description", "created_at"}).
-		AddRow(2, uuid.New(), "Marda para teste 1", time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC)).
-		AddRow(2, uuid.New(), "Marda para teste 2", time.Date(2022, time.February, 2, 22, 32, 42, 0, time.UTC))
+		AddRow(2, uuid.New(), "Marca para teste 1", time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC)).
+		AddRow(2, uuid.New(), "Marca para teste 2", time.Date(2022, time.February, 2, 22, 32, 42, 0, time.UTC))
 
 	// cria um mock da query executada
 	mock.ExpectQuery("SELECT (.+) FROM brand").WillReturnRows(rows).RowsWillBeClosed()
@@ -56,9 +56,39 @@ func TestFindAll_Should_Return_All_Brands(t *testing.T) {
 	assert.Equal(t, "id ASC", result.Sort)
 	assert.Equal(t, 2, result.NumberOfElements)
 	assert.NotNil(t, brands[0].ID)
-	assert.Equal(t, "Marda para teste 1", brands[0].Description)
+	assert.Equal(t, "Marca para teste 1", brands[0].Description)
 	assert.Equal(t, time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC), brands[0].CreatedAt)
 	assert.NotNil(t, brands[1].ID)
-	assert.Equal(t, "Marda para teste 2", brands[1].Description)
+	assert.Equal(t, "Marca para teste 2", brands[1].Description)
 	assert.Equal(t, time.Date(2022, time.February, 2, 22, 32, 42, 0, time.UTC), brands[1].CreatedAt)
+}
+
+// Deve retornar uma marca ao buscar por id
+func TestFindById_Should_Return_A_Brand_When_Searching_By_ID(t *testing.T) {
+	// cria um mock para conexao com o banco de dados
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	connector := mocks.NewMockDatabaseConnector(ctrl)
+
+	// cria um mock do banco de dados
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	mock.ExpectPing()
+
+	// cria um mock dos dados que deveram ser retornados
+	rows := sqlmock.NewRows([]string{"id", "description", "created_at"}).
+		AddRow(uuid.New(), "Marca para teste 1", time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC))
+
+	// cria um mock da query executada
+	mock.ExpectQuery("SELECT (.+) FROM brand WHERE id").WillReturnRows(rows).RowsWillBeClosed()
+	connector.EXPECT().OpenConnection().Return(db)
+
+	result, err := brand_repository.NewBrandRepository(connector).FindById(uuid.New())
+	assert.NoError(t, err)
+
+	// verifica os resultados
+	assert.NotNil(t, result.ID)
+	assert.Equal(t, "Marca para teste 1", result.Description)
+	assert.Equal(t, time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC), result.CreatedAt)
 }
