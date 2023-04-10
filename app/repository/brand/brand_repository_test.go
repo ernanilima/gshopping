3233ab3078068e912e781afb1e3439c3e3179af7
ernanilima <ernani.tecc@gmail.com>
2023-04-10 19:18:34 -0300

    Teste para buscar marca por descricao no depository

--- app/repository/brand/brand_repository_test.go
+++ app/repository/brand/brand_repository_test.go
@@ -92,3 +92,52 @@ func TestFindById_Should_Return_A_Brand_When_Searching_By_ID(t *testing.T) {
 	assert.Equal(t, "Marca para teste 1", result.Description)
 	assert.Equal(t, time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC), result.CreatedAt)
 }
+
+// Deve retornar as marcas localizadas ao buscar por descricao
+func TestFindAll_Should_Return_The_Brands_Found_When_Searching_By_Description(t *testing.T) {
+	// cria um mock para conexao com o banco de dados
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	connector := mocks.NewMockDatabaseConnector(ctrl)
+
+	// cria um mock do banco de dados
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	mock.ExpectPing()
+
+	// cria um mock dos dados que deveram ser retornados
+	rows := sqlmock.NewRows([]string{"count", "id", "description", "created_at"}).
+		AddRow(2, uuid.New(), "Marca para teste 1", time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC)).
+		AddRow(2, uuid.New(), "Marca para teste 2", time.Date(2022, time.February, 2, 22, 32, 42, 0, time.UTC))
+
+	// cria um mock da query executada
+	mock.ExpectQuery("SELECT (.+) FROM brand WHERE description").WillReturnRows(rows).RowsWillBeClosed()
+	connector.EXPECT().OpenConnection().Return(db)
+
+	result, err := brand_repository.NewBrandRepository(connector).FindByDescription("teste", utils.Pageable{
+		Size: 10,
+		Sort: "id ASC",
+	})
+	assert.NoError(t, err)
+
+	// converte para a entidade existente em content
+	bts, err := json.Marshal(result.Content)
+	assert.NoError(t, err)
+	var brands []model.Brand
+	err = json.Unmarshal(bts, &brands)
+	assert.NoError(t, err)
+
+	// verifica os resultados
+	assert.Equal(t, 0, result.TotalPages)
+	assert.Equal(t, 2, result.TotalElements)
+	assert.Equal(t, 10, result.Size)
+	assert.Equal(t, "id ASC", result.Sort)
+	assert.Equal(t, 2, result.NumberOfElements)
+	assert.NotNil(t, brands[0].ID)
+	assert.Equal(t, "Marca para teste 1", brands[0].Description)
+	assert.Equal(t, time.Date(2021, time.January, 1, 21, 31, 41, 0, time.UTC), brands[0].CreatedAt)
+	assert.NotNil(t, brands[1].ID)
+	assert.Equal(t, "Marca para teste 2", brands[1].Description)
+	assert.Equal(t, time.Date(2022, time.February, 2, 22, 32, 42, 0, time.UTC), brands[1].CreatedAt)
+}
