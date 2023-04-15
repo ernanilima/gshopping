a925eb3414d27579ebeb999bfc632223e0972d8d
ernanilima <ernani.tecc@gmail.com>
2023-04-15 12:28:41 -0300

    Atualizado CI - Validar cobertura

--- app/repository/database/connection_test.go
+++ app/repository/database/connection_test.go
@@ -1,16 +1,23 @@
 package database_test
 
 import (
+	"context"
+	"testing"
+
+	"github.com/ernanilima/gshopping/app/repository/database"
+	"github.com/ernanilima/gshopping/app/test/helpers"
+	"github.com/stretchr/testify/assert"
+
 	_ "github.com/lib/pq"
 )
 
-// func TestOpenConnection_Success(t *testing.T) {
-// 	configs := helpers.GetConfigsForIntegrationTesting(context.Background())
+func TestOpenConnection_Success(t *testing.T) {
+	configs := helpers.GetConfigsForIntegrationTesting(context.Background())
 
-// 	databaseConfig := &database.DatabaseConfig{Config: configs}
-// 	conn := databaseConfig.OpenConnection()
-// 	defer conn.Close()
+	databaseConfig := &database.DatabaseConfig{Config: configs}
+	conn := databaseConfig.OpenConnection()
+	defer conn.Close()
 
-// 	// verifica os resultados
-// 	assert.NotNil(t, conn)
-// }
+	// verifica os resultados
+	assert.NotNil(t, conn)
+}
