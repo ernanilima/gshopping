03bfc348d340e73c510de5ba37703f9e390d1ae1
ernanilima <ernani.tecc@gmail.com>
2023-06-29 19:11:19 -0300

    Cross origin

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ernanilima/gshopping/app/config"
 	"github.com/ernanilima/gshopping/app/repository/database"
 	"github.com/ernanilima/gshopping/app/router"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -26,9 +27,13 @@ func main() {
 	// inicia as rotas
 	routes := router.StartRoutes(controller)
 
+	handler := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	}).Handler(routes)
+
 	// inicia o banner
 	configs.StartBanner()
 
 	// executa a aplicacao
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", configs.Server.Port), routes))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", configs.Server.Port), handler))
 }
