13f3e3d798862ba113e962726191dd377d5f0f78
ernanilima <ernani.tecc@gmail.com>
2023-09-11 17:57:17 -0300

    Testes para rotas de marcas

--- app/router/brand_router_test.go
+++ app/router/brand_router_test.go
@@ -11,6 +11,60 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
+// Deve acessar a rota e retornar status code 200 ao inserir uma marca
+func TestRouteInsertBrand_Should_Access_The_Route_And_Return_Status_Code_200_When_Insert_A_Brand(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	controller := mocks.NewMockController(ctrl)
+	controller.EXPECT().InsertBrand(gomock.Any(), gomock.Any()).AnyTimes().Return()
+
+	r := router.StartRoutes(controller)
+
+	req := httptest.NewRequest("POST", "/v1/marca", nil)
+	res := httptest.NewRecorder()
+
+	routeMarca := getRouteByName(r.Routes(), req.RequestURI)
+	routeMarca.Handlers[req.Method].ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code, "deveria retornar o status code 200")
+}
+
+// Deve acessar a rota e retornar status code 200 ao editar uma marca
+func TestRouteEditBrand_Should_Access_The_Route_And_Return_Status_Code_200_When_Edit_A_Brand(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	controller := mocks.NewMockController(ctrl)
+	controller.EXPECT().EditBrand(gomock.Any(), gomock.Any()).AnyTimes().Return()
+
+	r := router.StartRoutes(controller)
+
+	req := httptest.NewRequest("PUT", "/v1/marca/6f75b5bc-e561-4bc7-a28d-e74bc706a4e9", nil)
+	res := httptest.NewRecorder()
+
+	routeMarca := getRouteByName(r.Routes(), "/v1/marca/{id}")
+	routeMarca.Handlers[req.Method].ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code, "deveria retornar o status code 200")
+}
+
+// Deve acessar a rota e retornar status code 200 ao deletar uma marca
+func TestRouteDeleteBrand_Should_Access_The_Route_And_Return_Status_Code_200_When_Delete_A_Brand(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	controller := mocks.NewMockController(ctrl)
+	controller.EXPECT().DeleteBrand(gomock.Any(), gomock.Any()).AnyTimes().Return()
+
+	r := router.StartRoutes(controller)
+
+	req := httptest.NewRequest("DELETE", "/v1/marca/6f75b5bc-e561-4bc7-a28d-e74bc706a4e9", nil)
+	res := httptest.NewRecorder()
+
+	routeMarca := getRouteByName(r.Routes(), "/v1/marca/{id}")
+	routeMarca.Handlers[req.Method].ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code, "deveria retornar o status code 200")
+}
+
 // Deve acessar a rota e retornar status code 200 ao buscar todas as marcas
 func TestRouteFindAllBrands_Should_Access_The_Route_And_Return_Status_Code_200_When_Fetch_All_Brands(t *testing.T) {
 	ctrl := gomock.NewController(t)
